cebdbe7bd0ab72c51b04179998ed89208438d037
Dinuka De Silva <l.dinukadesilva@gmail.com>
2022-10-21 04:05:55 -0400

    Parse links

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
 import HomeView from "./views/HomeView.vue";
 
 export default {
-  components: {HomeView}
+  components: {HomeView},
+  mounted() {
+    this.$el.outerHTML = this.$el.outerHTML
+        .replaceAll(/DisasterAWARE/g, "<a href='https://www.pdc.org/disasteraware/'>DisasterAWARE</a>")
+        .replaceAll(/PDC/g, "<a href='https://www.pdc.org/'>PDC</a>");
+  }
 }
 </script>
 
